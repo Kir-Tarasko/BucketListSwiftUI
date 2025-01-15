3c507567514dc74390251138365d624713bf07a4
Kirill Tarasko <tarasko.kirill@icloud.com>
2025-01-15 21:32:43 +0200

    textfield

--- BucketListSwiftUI/ContentView.swift
+++ BucketListSwiftUI/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var listItemName: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("", text: $listItemName)
         }
         .padding()
     }
