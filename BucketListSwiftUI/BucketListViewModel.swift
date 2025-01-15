ac72e21aad723c292b4de71dc862de2605b56ff6
Kirill Tarasko <tarasko.kirill@icloud.com>
2025-01-15 22:16:20 +0200

    Update BucketListViewModel.swift

--- BucketListSwiftUI/BucketListViewModel.swift
+++ BucketListSwiftUI/BucketListViewModel.swift
@@ -1,7 +1,15 @@
 //
-//  BucketListViewModel.swift
+//  BucketListItemModel.swift
 //  BucketListSwiftUI
 //
 //  Created by Kirill Tarasko on 15.01.2025.
 //
 
+import Foundation
+
+
+struct BucketListItemModel {
+    var name: String
+    var id = UUID()
+    var isCompleted: Bool = false
+}
